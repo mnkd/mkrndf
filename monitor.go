c8fde8a5a39c9213eeadf6025cb31d1e72110f4d
m-nakada <m-nakada@nekojarashi.com>
2017-01-08 23:36:23 +0900

    Rename total to size

--- monitor.go
+++ monitor.go
@@ -7,19 +7,19 @@ import (
 
 type MonitorWriter struct {
 	w     io.Writer
-	total int64
+	size  int64
 	count int64
 }
 
 func (m *MonitorWriter) Write(p []byte) (int, error) {
 	l := int64(len(p))
 	m.count += l
-	progress := float64(m.count) / float64(m.total) * float64(100)
+	progress := float64(m.count) / float64(m.size) * float64(100)
 	str := fmt.Sprintf("\r%0.1f %%", progress)
 	m.w.Write([]byte(str))
 	return int(l), nil
 }
 
-func NewMonitorWriter(w io.Writer, total int64) *MonitorWriter {
-	return &MonitorWriter{w, total, 0}
+func NewMonitorWriter(w io.Writer, size int64) *MonitorWriter {
+	return &MonitorWriter{w, size, 0}
 }
